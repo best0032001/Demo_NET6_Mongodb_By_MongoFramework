a37f0ba3c92424899eaf99df66dd9c0cd1c36a02
cheewin.b <cheewin.b@cmu.ac.th>
2023-08-18 14:48:56 +0700

    feat: poc StartSession

--- Demo_NET6_Mongodb_By_MongoFramework/Controllers/BookController.cs
+++ Demo_NET6_Mongodb_By_MongoFramework/Controllers/BookController.cs
@@ -1,8 +1,9 @@
 using Demo_NET6_Mongodb_By_MongoFramework.Models;
 using Demo_NET6_Mongodb_By_MongoFramework.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
-using MongoDB.Driver;
 using System.Net;
+using static System.Reflection.Metadata.BlobBuilder;
+
 
 namespace Demo_NET6_Mongodb_By_MongoFramework.Controllers;
 
@@ -10,60 +11,92 @@ namespace Demo_NET6_Mongodb_By_MongoFramework.Controllers;
 [ApiController]
 public class BookController : ControllerBase
 {
-  //  private readonly BookStoreDbContext _bookStoreDbContext;
-    private readonly IBookContext _bookContext;
-    public BookController( IBookContext bookContext)
+    private BookStoreDbContext _bookStoreDbContext;
+    public BookController(BookStoreDbContext bookStoreDbContext)
     {
-      //  _bookStoreDbContext = bookStoreDbContext;
-        _bookContext = bookContext;
+        _bookStoreDbContext = bookStoreDbContext;
     }
     [HttpGet("books")]
     public async Task<ActionResult> GetBooks()
     {
-        List<Book> books = await _bookContext.Books.Find(p => true)
-                            .ToListAsync();
+        List<Book> books = _bookStoreDbContext.Books.ToList();
         return Ok(books);
     }
     [HttpGet("books/{bookId}")]
     public async Task<ActionResult> GetBooks(String bookId)
     {
-        List<Book> books =await _bookContext.Books.Find(w => w.Id == bookId).ToListAsync();
+        List<Book> books = _bookStoreDbContext.Books.Where(w => w.Id == bookId).ToList();
         return Ok(books);
     }
     [HttpDelete("books/{bookId}")]
     public async Task<ActionResult> DeleteBooks(String bookId)
     {
-        FilterDefinition<Book> filter = Builders<Book>.Filter.Eq(p => p.Id, bookId);
-        DeleteResult deleteResult = await _bookContext
-                                                       .Books
-                                                       .DeleteOneAsync(filter);
-        List<Book> books = await _bookContext.Books.Find(w => w.Id == bookId).ToListAsync();
-        return Ok(books);
+        using (var session = _bookStoreDbContext.Connection.Client.StartSession())
+        {
+            session.StartTransaction();
+            try
+            {
+
+                Book book = _bookStoreDbContext.Books.Where(w => w.Id == bookId).First();
+                _bookStoreDbContext.Books.Remove(book);
+                _bookStoreDbContext.SaveChanges();
+                session.CommitTransaction();
+                List<Book> books = _bookStoreDbContext.Books.ToList();
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+                session.AbortTransaction();
+                return BadRequest();
+
+            }
+        }
+
     }
     [HttpPut("books/{bookId}")]
     public async Task<ActionResult> UpdateBooks(String bookId, [FromBody] Book book)
     {
-        var updateResult = await _bookContext
-                            .Books
-                            .ReplaceOneAsync(filter: g => g.Id == book.Id, replacement: book);
 
-        //Book bookData = _bookStoreDbContext.Books.Where(w => w.Id == bookId).First();
-        //_bookStoreDbContext.Books.Remove(bookData);
-        //_bookStoreDbContext.SaveChanges();
-        //_bookStoreDbContext.Books.Add(book);
-        //_bookStoreDbContext.SaveChanges();
-        List<Book> books = await _bookContext.Books.Find(w => w.Id == bookId).ToListAsync();
-        return Ok(books);
+        using (var session = _bookStoreDbContext.Connection.Client.StartSession())
+        {
+            session.StartTransaction();
+            try
+            {
+                Book bookData = _bookStoreDbContext.Books.Where(w => w.Id == bookId).First();
+                _bookStoreDbContext.Books.Remove(bookData);
+                _bookStoreDbContext.SaveChanges();
+                _bookStoreDbContext.Books.Add(book);
+                _bookStoreDbContext.SaveChanges();
+                List<Book> books = _bookStoreDbContext.Books.ToList();
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+                session.AbortTransaction();
+                return BadRequest();
+            }
+        }
     }
 
     [HttpPost("books")]
     public async Task<ActionResult> AddBooks(Book book)
     {
-
-        await _bookContext.Books.InsertOneAsync(book);
-
-
-        List<Book> books = await _bookContext.Books.Find(w => w.Id == book.Id).ToListAsync();
-        return Ok(books);
+        using (var session = _bookStoreDbContext.Connection.Client.StartSession())
+        {
+            session.StartTransaction();
+            try
+            {
+                _bookStoreDbContext.Books.Add(book);
+                _bookStoreDbContext.Books.Add(book);
+                _bookStoreDbContext.SaveChanges();
+                List<Book> books = _bookStoreDbContext.Books.ToList();
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+                session.AbortTransaction();
+                return BadRequest();
+            }
+        }
     }
 }
